688ca495c0982ecde79ab352344e592ed37158d7
Bobbyc6231 <114589800+Bobbyc6231@users.noreply.github.com>
2022-09-28 09:31:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Bobby Chan"; // update the part BETWEEN the "quotes"
+let userName = "Bobbyc6231"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
